97342bf2cc32819b019671f84f8259a1fe22bc00
saikrishnatbijil <saisboxintech@hmail.com>
2023-01-14 11:39:49 +0530

    First Commit Tager is done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './container'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const OpenAI = require('openai');
+  const { Configuration, OpenAIApi} = OpenAI;
+  const [message, setMessage] = useState('')
+  const [tag, setTag] = useState('Hit GO! to see the magic')
+  const configuration = new Configuration({
+    apiKey: "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"
+  });
+  const openai = new OpenAIApi(configuration);
+
+  async function makeTag() {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: 'Write a Tagline for a '+message,
+      max_tokens: 20,
+      temperature: 0,
+    });
+    console.warn(response.data.choices[0].text)
+    setTag(response.data.choices[0].text)
+  }
+
+  function getData(val) {
+    console.warn(val.target.value)
+    setMessage(val.target.value)
+  }
+
+  function wte() {
+    alert('For the best results. Explain about your company/Product. Like for us it would be `a website that makes taglines for other companies`. Like wise Explain about your Company/Product')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      {/* Content */}
+      <div className='container'>
+        <center>
+          <div className='formDiv'>
+            <input className='textInput' onChange={getData} type="text" placeholder='Explain about your Company/Product' />
+            <button className='send' onClick={makeTag}>GO!</button>
+          </div>
+          <p onClick={wte}>What to enter for best results?</p>
+          <h1>{tag}</h1>
+        </center>
+      </div>
+      {/* Content */}
     </div>
   );
 }
---
+++ src/container/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+function navbar() {
+  return (
+    <div className='margin'>
+      <div className='navbar'>
+      <div className='navContent'>
+        <h1>Tager</h1>
+        <div className='tagline'>
+          <h2>We'll Help You Find the Perfect Tagline - Every Time!</h2>
+          <h5>-Created using Tager</h5>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default navbar
